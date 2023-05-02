bc4b313d4ab032cd7ee563da870d12a069d2c5eb
Somersault0023 <66203219+Somersault0023@users.noreply.github.com>
2023-05-02 19:30:46 +0200

    practica 3

---
+++ practica3/js/bookSearch.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import axios from 'axios';
+
+function BookSearch() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [bookList, setBookList] = useState([]);
+  const [error, setError] = useState('');
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.get('https://www.googleapis.com/books/v1/volumes?q=${searchTerm}');
+      const data = response.data;
+
+      if (data && data.items) {
+        setBookList(data.items);
+        setError('');
+      } else {
+        setBookList([]);
+        setError('No se encontraron resultados.');
+      }
+    } catch (error) {
+      console.error(error);
+      setBookList([]);
+      setError('Ocurrió un error al realizar la búsqueda.');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Buscador de Libros</h1>
+
+      <form onSubmit={handleSearch}>
+        <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Introduce el nombre del libro" />
+        <button type="submit">Buscar</button>
+      </form>
+
+      {error && <p>{error}</p>}
+
+      <div>
+        {bookList.map((book) => (
+          <div key={book.id}>
+            <img src={book.volumeInfo.imageLinks?.thumbnail} alt="Portada del libro" />
+            <h3>{book.volumeInfo.title}</h3>
+            <p>Autores: {book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : 'Autor desconocido'}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default BookSearch;
